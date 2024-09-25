b30f2a25f47dc3ce76573aaa26b0d65f5063691a
Camargo11-max <cristiancamargo001@gmail.com>
2024-09-24 23:18:00 -0500

    first commit venta

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,23 @@
+/**
+ * 1. Agregar el Htmal Y CSS para las tarjetas o cards (imagen, titulo, card de boostrap)
+ * 2. Agregar al boton de cada card un evento Onclick, metodo decir el id de seleccion, en el metodo guaradar informacion en el localstorage
+ * 3. Cunado se agrge el item del carrito mostrar la cantidad de item que agrega el usuario.
+ * 4. mostrar la totalidad de productos, o deaplegar el listado con un boton de ver carrito o finalizar compra, redireccionar el el icono del carrito,
+ *  mostrar los productos agregados por el usuario, imagen,titulo, precio ,cantidad.
+ */
+let carrito = localStorage.getItem('listaCarrito') || [];
+function agregarProducto(identificador){
+ carrito.push(identificador)   
+ localStorage.setItem('listaCarrito', carrito);
+ actualizarContadorProductos();
+}
 
-const productlist = document.getElementById('product-list');
+function actualizarContadorProductos(){
+    const cartCount = document.getElementById ('count-car');
+    cartCount.textContent = carrito.length;
+} 
+
+/*const productlist = document.getElementById('product-list');
 
 const fetchPromise = fetch(
     "./json/info-page.json",
@@ -17,7 +35,7 @@ fetchPromise
 console.log(data);
 data.forEach((product) => {
     const listItem = document.createElement("li");
-    listItem.textContent = `Nombre ${product.name}, Precio ${product.price}`;
+    listItem.textContent = `Nombre ${product.name}, Precio ${product.price}, imagen ${product.image}, tipo ${product.type}`;
     productlist.appendChild(listItem);  
 
 });
@@ -29,17 +47,4 @@ console.error (`Error al obtener los productos: ${error}`);
 const errorMesage = document.createElement('p');
 errorMesage.textContent = 'No se pudo obtener la informacion de los productos.';
 productlist.appendChild(errorMesage);
-});
-
-
-/*let carrito = localStorage.getItem('listaCarrito') || [];
-function agregarProducto(identificador){
- carrito.push(identificador)   
- localStorage.setItem('listaCarrito', carrito);
- actualizarContadorProductos();
-}
-
-function actualizarContadorProductos(){
-    const cartCount = document.getElementById ('count-car');
-    cartCount.textContent = carrito.length;
-} */
+}); */
